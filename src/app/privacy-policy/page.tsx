a91b0d520c6a9002d53b821a0c212c77f9f5a4a4
A. <afsansait@gmail.com>
2025-06-25 14:01:24 +0800

    Update page.tsx

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
