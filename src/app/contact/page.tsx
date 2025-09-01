cc643481aa67aa77e4048ae55302dd5baed28f32
Renz <renzogabriel.laporno-19@cpu.edu.ph>
2025-09-01 11:07:08 +0800

    add:deployment fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -22,7 +22,7 @@ const RightArrowIcon: React.FC<{ className?: string }> = ({
       strokeLinejoin="round"
       strokeWidth="2"
       d="M17 8l4 4m0 0l-4 4m4-4H3"
-    ></path>
+    ></path> 
   </svg>
 );
 
